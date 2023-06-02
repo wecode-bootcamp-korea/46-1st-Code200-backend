3fc84a5ac2775f6ddcac5cf51d96860edd631c25
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-02 11:24:23 +0900

    router 내용 수정 - 필요없는 부분 삭제2

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const productController = require("../controllers/productController");
 router.get("/:productId", productController.getProductDetail);
-router.get("/recent/:subcategoryId", productController.getProductRecentDate);
 
 module.exports = {
   router,
