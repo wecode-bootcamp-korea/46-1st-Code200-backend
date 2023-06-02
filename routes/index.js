7c80edc8b22a148a5853fe24f9cda59ebea84e80
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-02 14:36:24 +0900

    product detail Dao 쿼리문 수정

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,10 @@ const router = express.Router();
 const productRouter = require("./productRouter");
 router.use("/products", productRouter.router);
 
+const reviewRouter = require("./reviewRouter");
+router.use("/reviews", reviewRouter.router);
+
+const likeRouter = require("./likeRouter");
+router.use("/likes", likeRouter.router);
+
 module.exports = router;
