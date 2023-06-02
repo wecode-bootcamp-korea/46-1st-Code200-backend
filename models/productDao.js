1ee53f9788e922a615cd4de3278c2f597e03aa19
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-02 11:10:21 +0900

    멘토리뷰수정 및 쿼리문 수정

--- models/productDao.js
+++ models/productDao.js
@@ -4,32 +4,33 @@ const getProductDetail = async (productId) => {
   try {
     return await appDataSource.query(
       `
-      SELECT 
+      SELECT
       p.name, 
       p.price, 
       p.description, 
-      JSON_ARRAYAGG(
-        JSON_OBJECT("imageUrl", pi.image_url)) as image,
-      sb.id as subcategory,
-      GROUP_CONCAT(r.content) AS content, 
-      AVG(r.rating) AS average_rating
+      JSON_OBJECT("imageUrl", pi.image_url) as image,
+      c.name as category,
+      sb.name as subcategory,
+      JSON_ARRAYAGG(JSON_OBJECT("content", r.content)) AS content,
+      ROUND(AVG(r.rating),1) AS average_rating
       FROM products AS p
-      LEFT JOIN reviews AS r 
+      INNER JOIN reviews AS r 
       ON r.product_id = p.id
-      LEFT JOIN product_images as pi
-      ON pi.product_id = p.id 
-      LEFT JOIN categories as c
-      ON c.id
-      LEFT JOIN subcategories as sb
+      INNER JOIN product_images as pi
+      ON pi.product_id = p.id       
+      INNER JOIN subcategories as sb
       ON sb.id = p.subcategory_id 
-      WHERE p.id = 0
-      GROUP BY p.name, p.price, p.description, pi.image_url
+      INNER JOIN categories as c
+      ON c.id = sb.category_id
+      WHERE p.id = ?
+      GROUP BY p.name, p.price, p.description, pi.image_url, c.name, sb.name
 	`,
       [productId]
     );
   } catch (err) {
+    console.log(err);
     const error = new Error("INVALID_DETAILDATA");
-    error.statusCode = 500;
+    error.statusCode = 400;
     throw error;
   }
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const productController = require("../controllers/productController");
 router.get("/:productId", productController.getProductDetail);
+router.get("/recent/:subcategoryId", productController.getProductRecentDate);
 
 module.exports = {
   router,
