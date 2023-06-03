1038aedd95df1c96c35c744594506f7925dd2a81
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-03 16:35:15 +0900

    merge main

--- models/cartDao.js
+++ models/cartDao.js
@@ -1,23 +1,5 @@
 const { appDataSource } = require("./dataSource");
 
-const updateCartQuantity = async (quantity, cartId, userId) => {
-  try {
-    return await appDataSource.query(
-      `
-      UPDATE carts
-      SET quantity = ?
-      WHERE carts.id = ? AND user_id = ? 
-    `,
-      [quantity, cartId, userId]
-    );
-  } catch (err) {
-    console.log(err);
-    const error = new Error("INVALID_UPDATE_CART_DATA");
-    error.statusCode = 400;
-    throw error;
-  }
-};
-
 const deleteCartList = async (cartId) => {
   try {
     return await appDataSource.query(
@@ -35,6 +17,5 @@ const deleteCartList = async (cartId) => {
 };
 
 module.exports = {
-  updateCartQuantity,
   deleteCartList,
 };
