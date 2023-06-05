57a2c1f8f79ed7a3d298833e9c39766b75f2ae09
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-06 01:37:05 +0900

    FIX: console.log제거

---
+++ models/likeDao.js
@@ -0,0 +1,39 @@
+const { appDataSource } = require("./dataSource");
+
+const createLike = async (userId, productId) => {
+  try {
+    return await appDataSource.query(
+      `INSERT INTO likes(
+            users_id,
+            product_id
+        ) VALUES (?, ?);
+        `,
+      [userId, productId]
+    );
+  } catch (err) {
+    const error = new Error("INVALID_LIKEDATA_INPUT");
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
+const deleteLike = async (userId, productId) => {
+  try {
+    return await appDataSource.query(
+      `
+      DELETE FROM likes
+       WHERE users_id = ? AND product_id = ?
+       `,
+      [userId, productId]
+    );
+  } catch (err) {
+    const error = new Error("INVALID_LIKE_DELETE");
+    error.statusCode = 400;
+    throw error;
+  }
+};
+
+module.exports = {
+  createLike,
+  deleteLike,
+};
