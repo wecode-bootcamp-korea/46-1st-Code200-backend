a443d7545b67d653877095eda6fb7e1a7b4f86ad
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-06 01:06:28 +0900

    FIX: 쿼리문 수정

--- models/productDao.js
+++ models/productDao.js
@@ -2,29 +2,31 @@ const { appDataSource } = require("./dataSource");
 
 const getProductDetail = async (productId) => {
   try {
-    const [product] = await appDataSource.query(
+    const product = await appDataSource.query(
       `
       SELECT
-      p.name, 
-      p.price, 
-      p.description,
-      (
-        SELECT ROUND(AVG(r.rating), 1) FROM reviews WHERE r.product_id = p.id
-      )as average,
-      JSON_ARRAYAGG(pi.image_url) as imageUrls,
-      c.name as category,
-      sb.name as subcategory
+        p.name,
+        p.price,
+        p.description,
+        JSON_ARRAYAGG(pi.image_url) as imageUrls,
+        (SELECT ROUND(AVG(reviews.rating), 1) FROM reviews WHERE reviews.product_id = p.id) as average,
+        sb.name as subcategory,
+        c.name as category
       FROM products AS p
-      LEFT JOIN reviews as r
-        ON r.product_id = p.id
-      INNER JOIN product_images as pi
-        ON pi.product_id = p.id
-      INNER JOIN subcategories as sb
-        ON sb.id = p.subcategory_id 
-      INNER JOIN categories as c
-        ON c.id = sb.category_id
-      WHERE p.id = ?
-      GROUP BY p.id
+      INNER JOIN
+        product_images AS pi
+      ON
+        pi.product_id = p.id
+      INNER JOIN 
+      	subcategories as sb
+      ON 
+      	sb.id = p.subcategory_id 
+      INNER JOIN 
+      	categories as c
+      ON 
+      	c.id = sb.category_id
+      WHERE
+        p.id = ?
 	  `,
       [productId]
     );
@@ -39,19 +41,13 @@ const getProductDetail = async (productId) => {
         FROM reviews r
         INNER JOIN users u ON u.id = r.user_id
         WHERE r.product_id = ?
-    `,
+      `,
       [productId]
     );
+    const reviewCount = reviews.length;
 
-    return { product, reviews };
+    return { product, reviews, reviewCount };
   } catch (err) {
-    console.log(
-      "🚀 ----------------------------------------------------------🚀"
-    );
-    console.log("🚀 | file: productDao.js:48 | getProductDetail | err:", err);
-    console.log(
-      "🚀 ----------------------------------------------------------🚀"
-    );
     const error = new Error("INVALID_DETAILDATA");
     error.statusCode = 400;
     throw err;
