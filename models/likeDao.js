72d36959926d7b432449d89496225bdbade4d7a6
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-06 01:40:09 +0900

    FIX: console.log 제거

--- models/likeDao.js
+++ models/likeDao.js
@@ -11,9 +11,6 @@ const createLike = async (userId, productId) => {
       [userId, productId]
     );
   } catch (err) {
-    console.log("🚀 -------------------------------------------------🚀");
-    console.log("🚀 | file: likeDao.js:14 | createLike | err:", err);
-    console.log("🚀 -------------------------------------------------🚀");
     const error = new Error("INVALID_LIKEDATA_INPUT");
     error.statusCode = 500;
     throw error;
