22855b8974e09824ff769b85fe86209019f67145
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-06-05 17:02:09 +0900

    멘토 리뷰 수정5

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,31 +4,25 @@ const { catchAsync } = require("../middleware/error");
 const getCartList = catchAsync(async (req, res) => {
   const userId = req.user.id;
   const cartsData = await cartService.getCartList(userId);
+
   return res.status(200).json({ data: cartsData });
 });
 
 const updateCartQuantity = catchAsync(async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const { quantity } = req.body;
-    const cartId = req.params.cartId;
+  const userId = req.user.id;
+  const { quantity } = req.body;
+  const cartId = req.params.cartId;
 
-    const cart = await cartService.updateCartQuantity(userId, quantity, cartId);
+  const cart = await cartService.updateCartQuantity(userId, quantity, cartId);
 
-    return res.status(200).json({ cart });
-  } catch (err) {
-    return res.status(err.statusCode || 400).json({ message: err.message });
-  }
+  return res.status(200).json({ cart });
 });
 
 const deleteCartItems = catchAsync(async (req, res) => {
-  try {
-    const { cartId } = req.query;
-    await cartService.deleteCartItems(cartId);
-    return res.status(204).send();
-  } catch (err) {
-    return res.status(err.statusCode || 500).json({ message: err.message });
-  }
+  const { cartId } = req.query;
+  await cartService.deleteCartItems(cartId);
+
+  return res.status(204).send();
 });
 
 module.exports = {
