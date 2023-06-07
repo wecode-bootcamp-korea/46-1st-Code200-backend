e0846877051bc78eeb5184758d682a5ec5641598
Gaffeecate <unitnatis123@gmail.com>
2023-06-07 16:07:13 +0900

    order, listrouter 삭제

--- routes/listRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const listController = require("../controllers/listController");
-const router = express.Router();
-
-router.get("", listController.getProductList);
-
-module.exports = {
-  router,
-};
